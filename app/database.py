543a463cb32c674208a76f19d2c07f2543f603c3
Kushagra Jaiswal <61771073+Nerd-coderZero@users.noreply.github.com>
2025-04-25 16:29:33 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,26 @@
 # app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.config import settings
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Check if we're running tests
+TESTING = os.environ.get("TESTING", "False").lower() == "true"
+
+# Use a test database during tests or the main database in normal operation
+if TESTING:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
